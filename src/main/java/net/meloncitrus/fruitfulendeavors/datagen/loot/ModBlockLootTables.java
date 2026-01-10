7f59203b63df664cc6711716ac0272b42fca3b3b
tired-melon <178475351+tired-melon@users.noreply.github.com>
2026-01-10 01:06:30 -0500

    Added tile and pattern blocks + datagen

--- src/main/java/net/meloncitrus/fruitfulendeavors/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/meloncitrus/fruitfulendeavors/datagen/loot/ModBlockLootTables.java
@@ -14,6 +14,7 @@ import java.util.Set;
 public class ModBlockLootTables  extends BlockLootSubProvider {
     public ModBlockLootTables(Set<Item> pExplosionResistant, FeatureFlagSet pEnabledFeatures) {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+
     }
 
     @Override
